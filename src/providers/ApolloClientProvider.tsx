6ff69ac520a5e7e7bad4780abe74a809fe3d8b23
hussain <hussainabdullatif@marun.edu.tr>
2024-01-05 02:05:14 +0300

    Fixed wrong uri

--- src/providers/ApolloClientProvider.tsx
+++ src/providers/ApolloClientProvider.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { PropsWithChildren } from "react";
 
 const client = new ApolloClient({
-    uri: "https://dashboard.stepzen.com/explorer?endpoint=api%2Ffamous-hyena",
+    uri: "https://northridgeville.stepzen.net/api/famous-hyena/__graphql",
     headers: {
         Authorization:
             "apikey northridgeville::stepzen.net+1000::5e7795abb5454ad9731a0e1e886fe3f3df8c34c0733b7651b90f73d419a036c1",
