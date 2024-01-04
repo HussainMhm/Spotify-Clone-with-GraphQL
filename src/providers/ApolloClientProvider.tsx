e4526d95bfc1a251a90a7048e2a71e15fd057f2a
hussain <hussainabdullatif@marun.edu.tr>
2024-01-05 01:45:40 +0300

    Setup Apollo Client

---
+++ src/providers/ApolloClientProvider.tsx
@@ -0,0 +1,17 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { PropsWithChildren } from "react";
+
+const client = new ApolloClient({
+    uri: "https://dashboard.stepzen.com/explorer?endpoint=api%2Ffamous-hyena",
+    headers: {
+        Authorization:
+            "apikey northridgeville::stepzen.net+1000::5e7795abb5454ad9731a0e1e886fe3f3df8c34c0733b7651b90f73d419a036c1",
+    },
+    cache: new InMemoryCache(),
+});
+
+const ApolloClientProvider = ({ children }: PropsWithChildren) => {
+    return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default ApolloClientProvider;
